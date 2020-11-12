d3d2ec1140ffff41dc548c5be2ee6900b288647d
Ashish <54847725+ashish807@users.noreply.github.com>
2020-11-12 23:10:42 +0545

    Assignment 5 uploaded

---
+++ Assignment5_18103101/question3.js
@@ -0,0 +1,79 @@
+function question3(){
+
+string = prompt("Enter the Brackets")
+string = string.replace(/\s+/g, '');
+var len = string.length;
+
+class Stack{
+    constructor()
+    {
+        this.items =[];
+    }
+    push(element){
+        this.items.push(element);
+    }
+    pop(){
+        if(this.items.length==0){
+            return "Underflow";
+        }
+        return this.items.pop();
+    }
+}
+
+var stack = new Stack();
+var comp = 0;
+if (len%2==0){
+    for(var i=0; i<len;i++){
+        if(string[i]=='{' || string[i]=="[" || string[i]=="(")
+        {
+          stack.push(string[i]);
+        }
+
+        if(string[i]=='}' || string[i]==']' || string[i] == ')')
+        {
+            var a = stack.pop();
+            console.log(a);
+            if(a=="{"){
+                if(string[i]=="}"){
+                    document.getElementById("message").innerHTML="Processing...";
+                }
+                else{
+                    document.getElementById("message").innerHTML="false";
+                    comp=1;
+                    break;
+                }
+            }
+          else if(a=="["){
+               if(string[i]=="]"){
+                document.getElementById("message").innerHTML="Processing...";
+                }
+                else{
+                    document.getElementById("message").innerHTML="false";
+                    comp=1;
+                    break;
+                }
+          }
+          else if(a=="("){
+               if(string[i]==")"){
+                document.getElementById("message").innerHTML="Processing...";
+                }
+                else{
+                    document.getElementById("message").innerHTML="false";
+                    comp=1;
+                    break;
+                }
+          }
+          
+        }
+    }
+    if(comp==0){
+        document.getElementById("message").innerHTML="true";
+    }
+    
+}
+
+
+else{
+    document.getElementById("message").innerHTML="false";
+}
+}
